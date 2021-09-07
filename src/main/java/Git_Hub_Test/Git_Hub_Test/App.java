5ee3acf3ce90d8783cb2d4e60dd1a991ea4ea5dd
LeelaYama <leelabhargaviy@gmail.com>
2021-09-07 15:57:45 +1000

    First Check In

---
+++ src/main/java/Git_Hub_Test/Git_Hub_Test/App.java
@@ -0,0 +1,7 @@
+package Git_Hub_Test.Git_Hub_Test;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Git_Hub_Test/Git_Hub_Test/AppTest.java
@@ -0,0 +1,11 @@
+package Git_Hub_Test.Git_Hub_Test;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+   System.out.println("Hi");
+  }
+}
